07281ae9168b45e3950b1ec384c66491ac88519e
Nguyễn Hoàng Hải <20130166@st.hcmuaf.edu.vn>
2025-05-11 22:06:19 +0700

    fix bug feat: auth, change username => email

--- src/main/java/vn/edu/hcmaf/apigamestore/auth/AuthController.java
+++ src/main/java/vn/edu/hcmaf/apigamestore/auth/AuthController.java
@@ -10,6 +10,7 @@ import vn.edu.hcmaf.apigamestore.auth.dto.LoginResponseDto;
 import vn.edu.hcmaf.apigamestore.auth.dto.request.RefreshTokenRequestDto;
 import vn.edu.hcmaf.apigamestore.auth.dto.request.RegisterRequestDto;
 import vn.edu.hcmaf.apigamestore.common.dto.BaseResponse;
+import vn.edu.hcmaf.apigamestore.common.dto.ErrorResponse;
 import vn.edu.hcmaf.apigamestore.common.dto.SuccessResponse;
 import vn.edu.hcmaf.apigamestore.common.util.JwtUtil;
 import vn.edu.hcmaf.apigamestore.role.RoleEntity;
@@ -22,52 +23,60 @@ import vn.edu.hcmaf.apigamestore.user.UserService;
 @RequestMapping("/api/auth")
 @Validated
 public class AuthController {
-  private final AuthService authService;
-  private final RoleService roleService;
-  private final UserService userService;
-  private final JwtUtil jwtUtil;
+    private final AuthService authService;
+    private final RoleService roleService;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-  public AuthController(RoleService roleService, AuthService authService, UserService userService, JwtUtil jwtUtil) {
-    this.authService = authService;
-    this.roleService = roleService;
-    this.userService = userService;
-    this.jwtUtil = jwtUtil;
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity<BaseResponse> register(@RequestBody @Valid RegisterRequestDto request) {
-    log.info("Register attempt for user: {}", request);
-    RoleEntity roleEntity = roleService.findByName("USER");
-    if (roleEntity == null) {
-      roleEntity = roleService.save("USER");
+    public AuthController(RoleService roleService, AuthService authService, UserService userService, JwtUtil jwtUtil) {
+        this.authService = authService;
+        this.roleService = roleService;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
-    LoginResponseDto loginResponseDto = authService.register(request, roleEntity);
 
-    return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", loginResponseDto));
-  }
-  @PostMapping("/login")
-  public ResponseEntity<BaseResponse> login(@RequestBody @Valid LoginRequestDto request) {
-      log.info("Login attempt for user: {}", request);
-      UserEntity user = userService.getUserByEmail(request.getEmail());
-      LoginResponseDto dto = authService.login(request, user);
-      return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", dto));
-  }
-  @PostMapping("/refresh-token")
-  public ResponseEntity<BaseResponse> refreshToken(@RequestBody @Valid RefreshTokenRequestDto request) {
-    jwtUtil.validateToken(request.getRefreshToken());
-    String email = jwtUtil.getEmailFromToken(request.getRefreshToken());
-    UserEntity userEntity = userService.getUserByEmail(email);
-    LoginResponseDto loginResponseDto = authService.refreshToken(userEntity, request.getRefreshToken());
-    return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", loginResponseDto));
-  }
-  @PostMapping("/logout")
-  public ResponseEntity<BaseResponse> logout(@RequestHeader("Authorization") String token) {
-    String finalToken = token.replace("Bearer ", "");
-    jwtUtil.validateToken(finalToken);
-    String email = jwtUtil.getEmailFromToken(finalToken);
-    UserEntity userEntity = userService.getUserByEmail(email);
-    boolean result = authService.logout(userEntity);
+    @PostMapping("/register")
+    public ResponseEntity<BaseResponse> register(@RequestBody @Valid RegisterRequestDto request) {
+        log.info("Register attempt for user: {}", request);
+        RoleEntity roleEntity = roleService.findByName("USER");
+        if (roleEntity == null) {
+            roleEntity = roleService.save("USER");
+        }
+        LoginResponseDto loginResponseDto = authService.register(request, roleEntity);
 
-    return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", result));
-  }
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Reregister success", loginResponseDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponse> login(@RequestBody @Valid LoginRequestDto request) {
+        log.info("Login attempt for user: {}", request);
+        UserEntity user = userService.getUserByEmail(request.getEmail());
+        LoginResponseDto dto = authService.login(request, user);
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Login success", dto));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<BaseResponse> refreshToken(@RequestBody @Valid RefreshTokenRequestDto request) {
+        jwtUtil.validateToken(request.getRefreshToken());
+        String email = jwtUtil.getEmailFromToken(request.getRefreshToken());
+        UserEntity userEntity = userService.getUserByEmail(email);
+        if (userEntity == null) {
+            return ResponseEntity.badRequest().body(new ErrorResponse("400", "FAIL", "User not found"));
+        } else if (!request.getRefreshToken().equals(userEntity.getRefreshToken())) {
+            return ResponseEntity.badRequest().body(new ErrorResponse("400", "FAIL", "Invalid refresh token, please login again"));
+        }
+        LoginResponseDto loginResponseDto = authService.refreshToken(userEntity, request.getRefreshToken());
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Get refresh-token success", loginResponseDto));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<BaseResponse> logout(@RequestHeader("Authorization") String token) {
+        String finalToken = token.replace("Bearer ", "");
+        jwtUtil.validateToken(finalToken);
+        String email = jwtUtil.getEmailFromToken(finalToken);
+        UserEntity userEntity = userService.getUserByEmail(email);
+        boolean result = authService.logout(userEntity);
+
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Logout success", result));
+    }
 }
