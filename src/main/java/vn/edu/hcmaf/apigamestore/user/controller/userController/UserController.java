6bb34da1d35a2db8e19663a4107262b6bbac3ec0
Nguyễn Hoàng Hải <20130166@st.hcmuaf.edu.vn>
2025-05-26 15:55:55 +0700

    add api: change pass for user. Admin can change any pass of any user

--- src/main/java/vn/edu/hcmaf/apigamestore/user/controller/userController/UserController.java
+++ src/main/java/vn/edu/hcmaf/apigamestore/user/controller/userController/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.hcmaf.apigamestore.common.response.BaseResponse;
+import vn.edu.hcmaf.apigamestore.common.response.ErrorResponse;
 import vn.edu.hcmaf.apigamestore.common.response.SuccessResponse;
 import vn.edu.hcmaf.apigamestore.user.UserEntity;
 import vn.edu.hcmaf.apigamestore.user.UserService;
@@ -27,6 +28,7 @@ import vn.edu.hcmaf.apigamestore.user.dto.UpdateUserDto;
 public class UserController {
     @Autowired
     private final UserService userService;
+
     /**
      * Retrieves the current user's information.
      *
@@ -36,8 +38,9 @@ public class UserController {
     @Operation(summary = "Get current user", description = "Retrieve the current user's information.")
     public ResponseEntity<BaseResponse> getCurrentUser() {
         UserEntity userEntity = userService.getCurrentUser();
-        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Get User info success", userEntity));
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", "Get User info success", userEntity));
     }
+
     /**
      * Updates the current user's information.
      *
@@ -46,10 +49,31 @@ public class UserController {
      */
     @PutMapping("/{userId}")
     @Operation(summary = "Update user", description = "Update the current user's information.")
-    public ResponseEntity<BaseResponse> updateUser(@RequestBody @Valid  UpdateUserDto updateUserDto, @PathVariable Long userId) {
-      UserEntity userEntity = userService.updateUser(updateUserDto, userId);
-      return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS","Update User Id: "+userId+ " success", userEntity));
+    public ResponseEntity<BaseResponse> updateUser(@RequestBody @Valid UpdateUserDto updateUserDto, @PathVariable Long userId) {
+        UserEntity userEntity = userService.updateUser(updateUserDto, userId);
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", "Update User Id: " + userId + " success", userEntity));
     }
+    /**
+     * Changes the password of the current user.
+     * This endpoint allows the user to change their password.
+     * It checks if the user ID matches the current user's ID or if the user has an ADMIN role.
+     *
+     * @param updateUserDto UpdateUserDto containing the new password.
+     * @param userId The ID of the user whose password is to be changed.
+     * @return ResponseEntity containing a success message wrapped in a SuccessResponse.
+     */
+    @PutMapping("/{userId}/change-password")
+    @Operation(summary = "Change user password", description = "Change the password of the current user.")
+    public ResponseEntity<BaseResponse> changePassword(@RequestBody @Valid UpdateUserDto updateUserDto, @PathVariable Long userId) {
+        // check if userId is current user or admin
+        UserEntity userEntity = userService.getCurrentUser();
+        if (!userEntity.getId().equals(userId) && !userEntity.getActiveRoles().stream().anyMatch(role -> role.getName().equals("ADMIN"))) {
+            return ResponseEntity.badRequest().body(new ErrorResponse("ERROR", "You can only change your own password", null));
+        }
+        userService.updatePassUser(updateUserDto.getPassword(), userId);
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", "Change password User Id: " + userId + " success",null));
+    }
+
     /**
      * Deletes a user by user ID.
      *
@@ -61,7 +85,7 @@ public class UserController {
     public ResponseEntity<BaseResponse> deleteUser(@PathVariable Long userId) {
         System.out.println("deleteUser = " + userId);
         boolean result = userService.deleteUser(userId);
-        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", "Delete User Id: "+userId+ " success",result));
+        return ResponseEntity.ok().body(new SuccessResponse<>("SUCCESS", "Delete User Id: " + userId + " success", result));
     }
 
 }
