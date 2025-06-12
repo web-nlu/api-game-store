d09b9b3f5ad1bbe7dd5c76147e0b050f89368201
Nguyễn Hoàng Hải <20130166@st.hcmuaf.edu.vn>
2025-06-12 17:15:09 +0700

    add feat: export revenue

--- src/main/java/vn/edu/hcmaf/apigamestore/sale_report/SaleReportController.java
+++ src/main/java/vn/edu/hcmaf/apigamestore/sale_report/SaleReportController.java
@@ -2,6 +2,7 @@ package vn.edu.hcmaf.apigamestore.sale_report;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -14,22 +15,24 @@ import vn.edu.hcmaf.apigamestore.common.response.SuccessResponse;
 
 import vn.edu.hcmaf.apigamestore.sale_report.dto.RevenueProjection;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin/sale-reports")
+@RequestMapping("/api/a/u/sale-reports")
 @RequiredArgsConstructor
 @Tag(name = "Sale Report", description = "Sale Report API for Admin")
 public class SaleReportController {
     private final SaleReportService saleReportService;
-    //GET http://localhost:8080/api/admin/sale-reports/revenue/epoch?type=DAY&startEpoch=1704067200&endEpoch=1746115199
-    //GET http://localhost:8080/api/admin/sale-reports/revenue/epoch?type=WEEK&startEpoch=1704067200&endEpoch=1746115199
-    //GET http://localhost:8080/api/admin/sale-reports/revenue/epoch?type=MONTH&startEpoch=1704067200&endEpoch=1746115199
+    private final ExportExcelService exportExcelService;
+    //GET http://localhost:8080/api/admin/sale-reports/revenue?type=DAY&startEpoch=1704067200&endEpoch=1746115199
+    //GET http://localhost:8080/api/admin/sale-reports/revenue?type=WEEK&startEpoch=1704067200&endEpoch=1746115199
+    //GET http://localhost:8080/api/admin/sale-reports/revenue?type=MONTH&startEpoch=1704067200&endEpoch=1746115199
     @Operation(summary = "Get Revenue by Epoch", description = "Get revenue report by epoch time")
-    @GetMapping("/revenue/epoch")
+    @GetMapping("/revenue")
     public ResponseEntity<BaseResponse> getRevenue(
             @RequestParam String type,
             @RequestParam Long startEpoch,
@@ -41,10 +44,33 @@ public class SaleReportController {
         LocalDateTime endDate = Instant.ofEpochSecond(endEpoch)
                 .atZone(ZoneId.systemDefault())
                 .toLocalDateTime();
-        System.out.println("Start Date: " + startDate);
-        System.out.println("End Date: " + endDate);
         return ResponseEntity.ok().body(
                 new SuccessResponse<>("SUCCESS", "Lấy báo cáo doanh thu thành công", saleReportService.getRevenue(startDate, endDate, type))
         );
+    }
+    @Operation(summary = "Export Revenue to Excel", description = "Export revenue report to Excel file")
+    @GetMapping("/revenue/export")
+    public ResponseEntity<Void> exportRevenueToExcel(
+            @RequestParam String type,
+            @RequestParam Long startEpoch,
+            @RequestParam Long endEpoch,
+            HttpServletResponse response
+    ) {
+
+        LocalDateTime startDate = Instant.ofEpochSecond(startEpoch)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        LocalDateTime endDate = Instant.ofEpochSecond(endEpoch)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+
+        List<RevenueProjection> revenues = saleReportService.getRevenue(startDate, endDate, type);
+        ExportExcelService exportExcelService = new ExportExcelService();
+        try {
+            exportExcelService.exportRevenueToExcel(revenues, response);
+        } catch (IOException e) {
+            throw new RuntimeException("Error exporting revenue to Excel", e);
+        }
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/vn/edu/hcmaf/apigamestore/sale_report/dto/RevenueProjection.java
+++ src/main/java/vn/edu/hcmaf/apigamestore/sale_report/dto/RevenueProjection.java
@@ -1,13 +1,7 @@
 package vn.edu.hcmaf.apigamestore.sale_report.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import vn.edu.hcmaf.apigamestore.order.repo.OrderRepository;
-
 import java.math.BigDecimal;
-import java.sql.Timestamp;
+
 import java.time.LocalDateTime;
 
 
