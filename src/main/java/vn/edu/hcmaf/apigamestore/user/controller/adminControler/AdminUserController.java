3644e711e62dfd440dae2730cd17f8422acf1d01
quangtho908 <quangtho23062002@gmail.com>
2025-06-16 12:46:54 +0700

    feat(permission) add permission for api admin/user

--- src/main/java/vn/edu/hcmaf/apigamestore/user/controller/adminControler/AdminUserController.java
+++ src/main/java/vn/edu/hcmaf/apigamestore/user/controller/adminControler/AdminUserController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.hcmaf.apigamestore.auth.AuthService;
@@ -28,6 +29,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/user")
 @RequiredArgsConstructor
+@PreAuthorize("hasAuthority('ADMIN')")
 @Validated
 /**
  * AdminUserController handles requests related to user management for administrators.
